ec12ca617e38b83ebe01bc52a536aff2dc92cdb4
Matt Lima <mlima@bizbash.com>
2013-11-01 17:02:53 -0400

    Add to cart apology

---
+++ dev/ajax/add-to-cart.php
@@ -0,0 +1,76 @@
+<?
+
+require('../includes/config.php');
+
+?>
+<div class="large-12 columns">
+	  			<h2>We are not taking orders over the internet yet. </h2>
+	  		</div>
+<p>Give us a call at 347-596-8096 or email us at <a href='mailto:hello@brittlebarn.com'>hello@brittlebarn.com</a> and we can take your order. We'll collect cash or run your card on pickup (within NYC), or run your card over the phone (for shipped orders).</p>
+
+
+<? exit(); ?><div class="row">
+
+	  		<div class="large-12 columns">
+	  			<h2>Item added to your cart</h2>
+	  		</div>
+
+	  		<div class="large-6 columns">
+
+	  				<p><strong>Veracruz Brittle</strong> <span>7oz bag</a></p>
+
+	  				<img src="http://placehold.it/100x100">
+
+	  				<table width="50%" border="0" cellspacing="0" cellpadding="0">
+					  <tr>
+					    <td><span class="shopping-cart-attribute">Quantity:</span></td>
+					    <td align="right"><input type="text" size="3" class="shopping-cart-qty"></td>
+					  </tr>
+					  <tr>
+					    <td><span class="shopping-cart-attribute">Price:</span></td>
+					    <td align="right"><span class="shopping-cart-attribute price">$9.00</span></td>
+					  </tr>
+					  <tr>
+					    <td><span class="shopping-cart-attribute">Shipping:</span></td>
+					    <td align="right"><span class="shopping-cart-attribute">$2.00</span></td>
+					  </tr>
+					</table>
+
+	  		</div>
+
+	  		<div class="large-6 columns">
+
+	  				<div class="panel cart-summary">
+
+	  					<p><strong>Cart summary	<span>1 Item</span></strong></p>
+
+	  					<hr>
+
+	  					<p>Subtotal	<span>$9.00</span></p>
+	  					<p>Shipping	<span>$6.00</span></p>
+	  					<p>Tax <i>Applied during checkout</i></p>
+
+	  					<hr>
+
+	  					<p><strong>Total <span class="green">$15.00</span></strong></p>
+
+	  				</div>
+
+	  		</div>
+
+
+	  		<div class="large-6 columns">
+
+	  				<a href="#" class="button round btn-continue-shopping expand">Continue Shopping</a>
+
+	  		</div>
+
+
+	  		<div class="large-6 columns">
+
+	  				<a href="#" class="button round expand">Go to Cart</a>
+
+	  		</div>
+
+
+	  </div>
